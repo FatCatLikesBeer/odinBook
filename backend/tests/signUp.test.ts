cf35b6bf761d51a5262a479e72c1bad7002bb3cc
Billy Bagel <itisbilly@gmail.com>
2024-07-08 23:52:59 -0700

    Wrote all tests for signup API

--- backend/tests/signUp.test.ts
+++ backend/tests/signUp.test.ts
@@ -43,7 +43,6 @@ describe('Successfull Signup', () => {
   });
 });
 
-// ---- POST REQUESTS ---- ////
 const apendage1: number = Math.floor(Math.random() * 1000000);
 describe('Bad Signups: preexisting', () => {
   const agent = request.agent(app);
@@ -71,6 +70,121 @@ describe('Bad Signups: preexisting', () => {
         userName: `delete_me_${apendage1}`,
         password: 'fakePassword',
         email: `itisbilly@gmail.com`,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBeDefined();
+    expect(parsedResult1.data).toBeNull();
+  });
+});
+
+const apendage2: number = Math.floor(Math.random() * 1000000);
+describe('Bad Signups: single empty field', () => {
+  const agent = request.agent(app);
+  it('Empty: userName', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        password: 'fakePassword',
+        email: `delete_me_${apendage2}@me.net`,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBeDefined();
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Empty: password', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        userName: `delete_me_${apendage2}`,
+        email: `itisbilly@gmail.com`,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBeDefined();
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Empty: email', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        userName: `delete_me_${apendage2}`,
+        password: 'fakePassword',
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBeDefined();
+    expect(parsedResult1.data).toBeNull();
+  });
+});
+
+const apendage3: number = Math.floor(Math.random() * 1000000);
+describe('Bad Signups: multiple empty fields', () => {
+  const agent = request.agent(app);
+  it('Empty: userName & password', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        email: `delete_me_${apendage3}@me.net`,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBeDefined();
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Empty: password & email', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        userName: `delete_me_${apendage3}`,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBeDefined();
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Empty: email & userName', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        password: 'fakePassword',
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBeDefined();
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Empty: all fields empty', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
       })
       .expect("Content-Type", /json/)
       .expect(400);

6e1cb642b3602cab8b091603bd44f62eb96076a1
Billy Bagel <itisbilly@gmail.com>
2024-07-10 00:38:34 -0700

    Login tests & controller created and finished

--- backend/controllers/logInController.ts
+++ backend/controllers/logInController.ts
@@ -1,28 +1,97 @@
 // Import Modules
-import { Request, Response } from 'express';
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+import { Request, Response, NextFunction } from 'express';
 import { ResponseJSON } from '../types/Responses';
+import * as jwt from 'jsonwebtoken';
+import { body, validationResult } from 'express-validator';
 
 // Import Models?
+import { User } from '../models/User';
+import { exists } from 'fs';
 
 // logIn Controller
 export const logInController: any = {};
-logInController.post = async (req: Request, res: Response) => {
-  // 1: use express body parser to check for form errors
-  // 2: If form data is fine, query database for userNames & email conflict
-  // 3: if none, then success
-  try {
-    const response: ResponseJSON = {
-      success: true,
-      message: "Login Controller not yet implemented",
-      data: {}
+
+const jwtSecret: string = String(process.env.JWT_SECRET);
+
+logInController.post = [
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email must not be empty'),
+
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password must not be empty'),
+
+  body('email')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Email or password is incorrect'),
+
+  body('email')
+    .trim()
+    .isLength({ max: 20 })
+    .withMessage('Email or password is incorrect'),
+
+  body('email')
+    .trim()
+    .isEmail()
+    .withMessage('Email or password is incorrect'),
+
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Email or password is incorrect'),
+
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // If errors exist
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessage = errors.array()[0].msg;
+        throw new Error(errorMessage);
+      }
+
+      const { email, password } = req.body;
+      const existingUser = await User.findOne({ where: { email } });
+      if (!existingUser) {
+        throw new Error('Email or password is incorrect');
+      }
+
+      const match = await bcrypt.compare(password, existingUser.dataValues.password);
+      if (!match) {
+        throw new Error('Email or password is incorrect');
+      }
+
+      const userAgent = req.headers['user-agent'];
+      const id: string = existingUser.dataValues.id;
+      const payload = { userAgent, id };
+
+      const response: ResponseJSON = {
+        success: true,
+        message: "Login Successful",
+        data: {}
+      }
+
+      jwt.sign(payload, jwtSecret, { expiresIn: '600s' }, (err, token) => {
+        if (err) {
+          throw new Error(String(err));
+        }
+
+        console.log(token);
+        res.cookie("Bearer", token).json(response);
+        next();
+      });
+    } catch (error) {
+      const failureResponse: ResponseJSON = {
+        success: false,
+        message: String(error),
+        data: null,
+      }
+      res.status(400).json(failureResponse);
     }
-    res.json(response);
-  } catch (error) {
-    const failureResponse: ResponseJSON = {
-      success: false,
-      message: `Error: ${error}`,
-      data: null,
-    }
-    res.json(failureResponse);
-  }
-}
+  },
+];
---
+++ backend/tests/logIn.test.ts
@@ -0,0 +1,144 @@
+require('dotenv').config();
+const request = require('supertest');
+import express from 'express';
+import { logInRouter } from '../routes/loginRouter';
+import sequelize from '../models/SequelizeConnection';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', logInRouter);
+
+beforeAll(async () => {
+  await sequelize.authenticate();
+});
+
+afterAll(async () => {
+  await sequelize.close();
+})
+
+const accountPassword: string = String(process.env.ACCPASSWORD);
+
+// Successful Login
+describe('Successfull Login', () => {
+  const agent = request.agent(app);
+  let cookie: any;
+  it('Proper values', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .set('User-Agent', 'JestSupertest/0.0')
+      .send({
+        email: 'itisbilly@gmail.com',
+        password: accountPassword,
+      })
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    cookie = await loginResponse.get('set-cookie')[0];
+    expect(cookie).toBeDefined();
+    expect(cookie.split("=")[0]).toMatch('Bearer');
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeTruthy();
+    expect(parsedResult1.message).toBe('Login Successful');
+    expect(parsedResult1.data).not.toBeNull();
+  });
+});
+
+describe('Bad values', () => {
+  const agent = request.agent(app);
+  it('Incorrect email', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        email: 'itisbilly@gmail.csldkjfwoieurom',
+        password: accountPassword,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBe('Error: Email or password is incorrect');
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Incorrect password', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        email: 'itisbilly@gmail.com',
+        password: `${accountPassword}lsiekdjf`,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBe('Error: Email or password is incorrect');
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Incorrect email & password', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        email: 'itisbilly@gmail.csldkjfwoieurom',
+        password: `${accountPassword}lsiekdjf`,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBe('Error: Email or password is incorrect');
+    expect(parsedResult1.data).toBeNull();
+  });
+});
+
+describe('Missing values', () => {
+  const agent = request.agent(app);
+  it('Missing email', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        password: accountPassword,
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBe('Error: Email must not be empty');
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Missing password', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({
+        email: 'itisbilly@gmail.com',
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBe('Error: Password must not be empty');
+    expect(parsedResult1.data).toBeNull();
+  });
+
+  it('Missing email & password', async () => {
+    const loginResponse = await agent
+      .post('/')
+      .send({})
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    const parsedResult1 = await JSON.parse(loginResponse.text);
+    expect(parsedResult1.success).toBeFalsy();
+    expect(parsedResult1.message).toBe('Error: Email must not be empty');
+    expect(parsedResult1.data).toBeNull();
+  });
+});
+

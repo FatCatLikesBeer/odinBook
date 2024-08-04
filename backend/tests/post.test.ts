9c639614dfbbbeb6b32510059ddf9fb50e527c2f
Billy Bagel <itisbilly@gmail.com>
2024-08-03 23:49:09 -0700

    Created expected API post tests for POST requests

--- backend/tests/post.test.ts
+++ backend/tests/post.test.ts
@@ -41,17 +41,159 @@ describe("POST requests", () => {
     expect(parsedResponse.success).toBeTruthy();
   });
 
-  it("POST text without title", async () => { });
-  it("POST text without description", async () => { });
-  it("POST text without title & description", async () => { });
-  it("POST text without owner id", async () => { });
-  it("POST link without title", async () => { });
-  it("POST link without description/link/JSON", async () => { });
-  it("POST image without title", async () => { });
-  it("POST image without description/JSON", async () => { });
-  it("Successful text POST", async () => { });
-  it("Successful link POST", async () => { });
-  it("Successful image POST", async () => { });
+  it("POST text without title", async () => {
+    const body = new BodyContent({ title: "" });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Invalid post title/);
+  });
+
+  it("POST text without description", async () => {
+    const body = new BodyContent({ body: "" });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Invalid post body/);
+  });
+
+  it("POST text without title & description", async () => {
+    const body = new BodyContent({ body: "", title: "" });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Invalid post title/);
+  });
+
+  it("POST text without owner id", async () => {
+    const body = new BodyContent({ ownerId: "invalidOwnerId" });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Owner Id Error/);
+  });
+
+  it("POST link without title", async () => {
+    const body = new BodyContent({ type: 'link', title: "" });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Invalid post title/);
+  });
+
+  it("POST link without body/link/JSON", async () => {
+    const body = new BodyContent({ body: "", type: 'link' });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Invalid post body/);
+  });
+
+  it("POST image without title", async () => {
+    const body = new BodyContent({ title: "", type: 'image' });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Invalid post title/);
+  });
+
+  it("POST image without description/JSON", async () => {
+    const body = new BodyContent({ type: 'image', body: "" });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(400)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeFalsy();
+    expect(parsedResponse.message).toMatch(/Error: Invalid post content/);
+  });
+
+  it("Successful text POST", async () => {
+    const body = new BodyContent({});
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeTruthy();
+    expect(parsedResponse.message).toMatch(/Post submitted!/);
+  });
+
+  it("Successful link POST", async () => {
+    const body = new BodyContent({ type: "link", body: 'http://www.example.com/' });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeTruthy();
+    expect(parsedResponse.message).toMatch(/Link Submitted!/);
+  });
+
+  it("Successful image POST", async () => {
+    const body = new BodyContent({ type: "image" });
+    const response = await agent
+      .post('/')
+      .send(body)
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    const parsedResponse = JSON.parse(response.text);
+    console.log(parsedResponse);
+    expect(parsedResponse.success).toBeTruthy();
+    expect(parsedResponse.message).toMatch(/Link Submitted!/);
+  });
 });
 
 // GET DETAIL Requests
@@ -135,3 +277,30 @@ describe("DELETE requests", () => {
 // 2) Text posts will be a regular post
 // 2) link posts will have the links in the body of the post
 // 2) image posts will have JSON in the body. An array of image locations
+
+interface UserOptions {
+  ownerId?: string;
+  title?: string;
+  body?: string;
+  type?: string; // text, image, link
+  privacy?: string | null;
+  visibility?: string | null;
+}
+
+class BodyContent {
+  ownerId: string;
+  title: string;
+  body: string;
+  type: string; // text, image, link
+  privacy: string | null;
+  visibility: string | null;
+
+  constructor({ ownerId = "1", title = "Here is a title", body = "Here is a body", type = "text", privacy = "public", visibility = "draft" }: UserOptions) {
+    this.ownerId = ownerId;
+    this.title = title;
+    this.body = type == "image" ? '{"images":["/images/mesmer/0","/images/mesmer/1","/images/mesmer/2"]}' : body;
+    this.type = type;
+    this.privacy = privacy;
+    this.visibility = visibility;
+  }
+}
